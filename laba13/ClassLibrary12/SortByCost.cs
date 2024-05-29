d3a13d1db4ede34b06882428c809268a9e5d0f84
D0ne1 <neelixer228@gmail.com>
2024-05-29 23:26:01 +0500

    всё сделал

---
+++ laba13/ClassLibrary12/SortByCost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary12
+{
+    public class SortByCost : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            Auto auto1 = x as Auto;
+            Auto auto2 = y as Auto;
+            if (auto1.Cost < auto2.Cost) return -1;
+            else if (auto1.Cost == auto2.Cost) return 0;
+            else return 1;
+        }
+    }
+}
